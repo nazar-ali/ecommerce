46ffb76144eeff9dfdc0fbfd0891f87eff63e6db
nazar_ali <nazaralibaloch3@gmail.com>
2026-02-12 23:14:10 +0500

    final commit

--- helper/db.js
+++ helper/db.js
@@ -1,14 +1,24 @@
 import mongoose from "mongoose";
-export const connectDb = async () => {
-  try {
-    const { connection } = await mongoose.connect(process.env.MONGO_DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Database connected...");
-    console.log(connection.host);
-  } catch (e) {
-    console.log("Database connection failed");
-    console.log(e);
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+
+if (!MONGO_DB_URL) {
+  throw new Error("Please define MONGO_DB_URL");
+}
+
+let cached = (global).mongoose;
+
+if (!cached) {
+  cached = (global).mongoose = { conn: null, promise: null };
+}
+
+export async function connectDb() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_DB_URL).then((mongoose) => mongoose);
   }
-};
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
