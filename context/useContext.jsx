e572d0d6b5400b65e7352b7e684f6480e73f6b6d
nazar_ali <nazaralibaloch3@gmail.com>
2026-02-14 05:30:08 +0500

    final build fixes

--- context/useContext.jsx
+++ context/useContext.jsx
@@ -74,7 +74,7 @@ export const AppContextProvider = (props) => {
     });
   };
   const handleLogout = async () => {
-    const res = await fetch("/api/logout", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/logout`, {
       method: "GET",
       credentials: "include",
     });
